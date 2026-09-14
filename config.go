fa43aa5d2caf657615f7ad47e464c54de75dbe9f
Ray Liu <raydatray@gmail.com>
2026-09-13 20:58:48 -0400

    [4/n][retry] map retry delays to topic partitions

--- config.go
+++ config.go
@@ -2,7 +2,9 @@ package kq
 
 import (
 	"errors"
+	"fmt"
 	"strings"
+	"time"
 
 	"github.com/twmb/franz-go/pkg/kgo"
 )
@@ -30,4 +32,23 @@ func (c Config) readyTopic() string {
 
 func (c Config) workerGroup() string {
 	return "kq." + c.Queue + ".workers"
+}
+
+func retryTopic(queue string, lower time.Duration) string {
+	return fmt.Sprintf("%s-retry-%s", queue, formatRetryDelay(lower))
+}
+
+func formatRetryDelay(delay time.Duration) string {
+	switch {
+	case delay == 0:
+		return "0s"
+	case delay%(24*time.Hour) == 0:
+		return fmt.Sprintf("%dd", delay/(24*time.Hour))
+	case delay%time.Hour == 0:
+		return fmt.Sprintf("%dh", delay/time.Hour)
+	case delay%time.Minute == 0:
+		return fmt.Sprintf("%dm", delay/time.Minute)
+	default:
+		return fmt.Sprintf("%ds", delay/time.Second)
+	}
 }
