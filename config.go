965ad1df21379c9ac1b2e90b8c03ca3044d57ae1
Ray Liu <raydatray@gmail.com>
2026-09-20 16:08:01 -0400

    [12/n][worker] add dlq

--- config.go
+++ config.go
@@ -76,3 +76,7 @@ func (c Config) retryTopics() []string {
 func (c Config) retryMoverGroup() string {
 	return "kq." + c.Queue + ".retry-mover"
 }
+
+func (c Config) deadLetterTopic() string {
+	return c.Queue + "-dlq"
+}
